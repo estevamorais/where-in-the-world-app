e5d0cd0322162c1abd463a5a160405e4bd0586d3
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2022-11-25 00:25:40 -0300

    Add link to Home in title

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,16 @@
 import "./index.scss";
 
+import { NavLink } from "react-router-dom";
+
 import SwitchThemeButton from "../SwitchThemeButton";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__content container">
-        <h1 className="navbar__title">Where in the world?</h1>
+        <h1 className="navbar__title">
+          <NavLink to="/">Where in the world?</NavLink>
+        </h1>
 
         <SwitchThemeButton />
       </div>
